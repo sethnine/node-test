42cc95529d8a86c55fa72c25162ba4961e93daba
Jordan Davie <jordandavie@tutanota.de>
2024-08-13 11:48:21 +1000

    inital commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+
+app.get('/', express.static('content'))
+
+app.get('/random', function (req, res) {
+    console.log(req.query);
+    let min = 0
+    if (! (typeof req.query.min == "undefined")) {
+        if (!isNaN(parseInt(req.query.min)))
+            min = parseInt(req.query.min)
+    }
+    let max = 100
+    if (! (typeof req.query.max == "undefined")) {
+        if (!isNaN(parseInt(req.query.max)))
+            max = parseInt(req.query.max)
+    }
+    const diff = Math.max(max,min)-Math.min(max,min);
+
+    const rn = Math.min(max,min)+Math.round(Math.random()*diff);
+    const rns = String(rn);
+  res.send(rns);
+})
+
+app.use('/query', express.urlencoded());
+app.post('/query', function (req, res) {
+    console.log(req.body)
+    res.send(req.body);
+}
+
+)
+
+app.listen(3000)
