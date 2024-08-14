748bd4c75e4c25ab1c6edfdc303ed8ea65cc2e15
Jordan Davie <jordandavie@tutanota.de>
2024-08-14 15:16:13 +1000

    added authentication

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
+const { log } = require("console");
 const express = require("express");
 const app = express();
 var session = require("express-session");
+const { isNull } = require("util");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const revision = require("child_process")
   .execSync("git rev-parse --short HEAD")
   .toString()
   .trim();
 
+let users = [
+  {
+    username: "jordan",
+    password: "$2b$10$w5W0CwSdfmuy5hMXMcxLEeTcl4rQH7fmGT1roT67mTY0XWr8smw/S", //password1
+  },
+  {
+    username: "jordan2",
+    password: "$2b$10$w5W0CwSdfmuy5hMXMcxLEeTcl4rQH7fmGT1roT67mTY0XWr8smw/S",
+  },
+];
 app.use(
   session({
-    secret: "CHANGEME",
+    secret: "CHANGEME1",
     cookie: {
       maxAge: 1000 * 60 * 30, // 30 minutes
     },
@@ -22,7 +36,7 @@ app.get("/endsession", (req, res) => {
         <script>setTimeout(()=>{window.location.replace("/")},5000)</script>`);
 });
 
-app.get("/", express.static("content"));
+app.use("/", express.static("content"));
 
 app.get("/visitCount", (req, res) => {
   if (req.session.visitCount == null) req.session.visitCount = 0;
@@ -30,105 +44,8 @@ app.get("/visitCount", (req, res) => {
   res.send(JSON.stringify({ visitCount: req.session.visitCount }));
 });
 
-// app.get("/", (req, res) => {
-//   let counter;
-//   if (!req.session.pageViews) {
-//     req.session.pageViews = 1;
-//     counter = `<h1> Welcome this is the first time you've visited this page!</h1>`;
-//   } else {
-//     req.session.pageViews++;
-//     counter = `This is the ${req.session.pageViews}th time you've viewed this page!`;
-//   }
-//   res.send(`<!DOCTYPE html>
-// <html lang="en">
-//   <head>
-//     <meta charset="UTF-8" />
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//     <title>Hello World</title>
-//     <script
-//       src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js"
-//       integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ=="
-//       crossorigin="anonymous"
-//       referrerpolicy="no-referrer"
-//     ></script>
-//     <link
-//       rel="stylesheet"
-//       href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css"
-//       integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg=="
-//       crossorigin="anonymous"
-//       referrerpolicy="no-referrer"
-//     />
-//     <script
-//       src="https://cdnjs.cloudflare.com/ajax/libs/vue/3.4.37/vue.global.min.js"
-//       integrity="sha512-DqEEvSuoZMoX7siGBL6dqybFZeH0oCysZnIXBq9PHttRbdEWwc7tMk0nlkGhEsLJOMzk1fnNU6OYe/o3V3D61Q=="
-//       crossorigin="anonymous"
-//       referrerpolicy="no-referrer"
-//     ></script>
-//   </head>
-//   <body>
-//     <div id="app" class="container">
-//       <header class="p-1 bg-secondary d-flex justify-content-end">
-//         <span class="text-light">Rev:{{siteRevision}}</span>
-//       </header>
-//       <h1>Hello World!</h1>
-//       <form @submit.prevent="getRandom()">
-//         <label
-//           >Min: <input type="number" name="min" pattern="[0-9]" v-model="min"
-//         /></label>
-//         <label
-//           >Max: <input type="number" name="max" pattern="[0-9]" v-model="max"
-//         /></label>
-//         <button class="btn btn-primary">Get Random</button>
-//         <p v-if="randomNumber!==null">The random number is: {{randomNumber}}</p>
-//       </form>
-//       <p>${counter}</p>
-//     </div>
-//     <script>
-//       const app = Vue.createApp({
-//         data() {
-//           return {
-//             siteRevision: null,
-//             min: 0,
-//             max: 100,
-//             randomNumber: null,
-//           };
-//         },
-//         methods: {
-//           getRandom() {
-//             fetch(\`/random?min=\${this.min}&max=\${this.max}\`)
-//               .then((r) => {
-//                 if (!r.ok) throw new Error("Could not fetch");
-//                 return r.text();
-//               })
-//               .then((d) => (this.randomNumber = d))
-//               .catch(console.error);
-//           },
-//           getCommitHash() {
-//             fetch("/rev")
-//               .then((r) => {
-//                 if (!r.ok) throw new Error("Could not fetch Revision");
-//                 return r.text();
-//               })
-//               .then((data) => {
-//                 if (this.siteRevision == null) this.siteRevision = data;
-//                 else if (this.siteRevision != data) location.reload();
-//               })
-//               .catch(console.error);
-//           },
-//         },
-//         mounted() {
-//           this.getCommitHash();
-//           setInterval(this.getCommitHash, 2500);
-//         },
-//       });
-//       vm = app.mount("#app");
-//     </script>
-//   </body>
-// </html>`);
-// });
-
 app.get("/random", function (req, res) {
-  console.log(req.query);
+  // console.log(req.query);
   let min = 0;
   if (!(typeof req.query.min == "undefined")) {
     if (!isNaN(parseInt(req.query.min))) min = parseInt(req.query.min);
@@ -146,13 +63,98 @@ app.get("/random", function (req, res) {
 
 app.use("/query", express.urlencoded());
 app.post("/query", function (req, res) {
-  console.log(req.body);
+  // console.log(req.body);
   res.send(req.body);
 });
 
 app.get("/rev", (req, res) => {
   res.send(revision);
 });
+
+app.use("/register", express.json({ limit: "1kb" }));
+app.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  // console.log(req.body);
+  if (typeof username === "undefined") {
+    // no username input
+    res.status(400).json({ error: "no username input" });
+    return;
+  }
+  if (typeof password === "undefined") {
+    // no password input
+    res.status(400).json({ error: "no password input" });
+    return;
+  }
+
+  // username already taken
+  if (users.find((u) => u.username === username)) {
+    res.status(409).json({ error: "username taken" });
+    return;
+  }
+
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    console.log(err);
+    // log(username, hash);
+    users.push({
+      username: username,
+      password: hash,
+    });
+  });
+
+  req.session.username = username;
+  res.status(200).json({ message: "Login successful" });
+});
+
+app.use("/login", express.json({ limit: "1mb" }));
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  // console.log(req.body);
+
+  if (typeof username === "undefined") {
+    // no username input
+    res.status(400).json({ error: "bad request!" });
+    return;
+  }
+  if (typeof password === "undefined") {
+    // no password input
+    res.status(400).json({ error: "bad request!" });
+    return;
+  }
+  const u = users.find((u) => u.username === req.body.username);
+  if (u === undefined) {
+    // no matching user in db
+    // console.log("cannot find user", username);
+    res
+      .status(401)
+      .json({ error: "cannot find matching user with matching credentials" });
+    return;
+  }
+  // console.log(u);
+  bcrypt.compare(password, u.password, (err, result) => {
+    // console.log(result);
+    if (err) {
+      console.error("bcrypt error", err);
+      res.status(500).json({ error: "internal server error" });
+      return;
+    }
+
+    if (!result) {
+      res.status(401).json({ error: "cannot find matching user creds" }); // wrong password
+      return;
+    }
+
+    req.session.username = username;
+    res.status(200).json({ message: "Login successful" });
+  });
+});
+
+app.use("/members", authCheck, express.static("members"));
+function authCheck(req, res, next) {
+  if (req.session.username) next();
+  else res.redirect("/login");
+}
 
 console.log("running git revision: " + revision);
 app.listen(3000);
