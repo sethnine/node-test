8b8748da5b46001f48de7114df8519e3bc0a26e3
Jordan Davie <jordandavie@tutanota.de>
2024-08-17 15:39:55 +1000

    implemented typescript support

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,84 @@
-const { log } = require("console");
-const express = require("express");
-const app = express();
-var session = require("express-session");
-const { isNull } = require("util");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-
-const { MongoClient } = require("mongodb");
+import express, { Express, NextFunction, Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import session from 'express-session';
+import { MongoClient } from "mongodb";
+import { error } from 'console';
 require("dotenv").config();
 
-const client = new MongoClient(process.env.dbURI);
-async function bob() {
-  await client.connect();
-  console.log("connected to server!");
-  const db = client.db(process.env.dbNAME);
-  const collection = db.collection(process.env.dbCOLLECTION);
-  // collection.insertMany(users);
-  console.log(await collection.countDocuments(), "users in users table");
+declare module "express-session" {
+  interface SessionData {
+    visitCount: number;
+    username: string;
+  }
 }
 
-bob();
-
-const revision = require("child_process")
-  .execSync("git rev-parse --short HEAD")
-  .toString()
-  .trim();
-
-// let users = [
-//   {
-//     username: "jordan",
-//     password: "$2b$10$w5W0CwSdfmuy5hMXMcxLEeTcl4rQH7fmGT1roT67mTY0XWr8smw/S", //password1
-//   },
-//   {
-//     username: "jordan2",
-//     password: "$2b$10$w5W0CwSdfmuy5hMXMcxLEeTcl4rQH7fmGT1roT67mTY0XWr8smw/S",
-//   },
-// ];
+const app = express();
 app.use(
   session({
     secret: "CHANGEME1",
+    resave: false,
+    saveUninitialized: true,
     cookie: {
       maxAge: 1000 * 60 * 30, // 30 minutes
     },
   })
 );
 
-app.get("/endsession", (req, res) => {
-  req.session.destroy();
-  res.send(`Your session has been destroyed, redirecting...
-        <script>setTimeout(()=>{window.location.replace("/")},5000)</script>`);
-});
+const saltRounds = 10;
 
+const SERV_PORT=process.env.SERV_PORT||3000;
+
+if (process.env.dbURI === undefined) {
+  console.log("dbURI is not set aborting!")
+  process.abort()
+}
+if (process.env.dbCOLLECTION === undefined) {
+  console.log("dbCOLLECTION is not set aborting!")
+  process.abort()
+}
+const client: MongoClient = new MongoClient(process.env.dbURI!);
+async function printUsercount() {
+  await client.connect();
+  console.log("connected to server!");
+  const db = client.db(process.env.dbNAME);
+  const collection = db.collection(process.env.dbCOLLECTION!);
+  console.log(await collection.countDocuments(), "users in users table");
+}
+
+printUsercount();
+
+// store running git revision
+const revision = require("child_process")
+  .execSync("git rev-parse --short HEAD")
+  .toString()
+  .trim();
+
+
+
+app.get("/endsession", (req: Request, res: Response) => {
+  req.session.destroy((err) => {
+    console.log(err);
+  });
+});
 app.use("/", express.static("content"));
 
-app.get("/visitCount", (req, res) => {
-  if (req.session.visitCount == null) req.session.visitCount = 0;
+app.get("/visitCount", (req: Request, res: Response) => {
+  if (typeof req.session.visitCount === "undefined") {
+    req.session.visitCount = 0;
+  }
   req.session.visitCount++;
   res.send(JSON.stringify({ visitCount: req.session.visitCount }));
 });
 
-app.get("/random", function (req, res) {
+app.get("/random", function (req: Request, res: Response) {
   // console.log(req.query);
   let min = 0;
   if (!(typeof req.query.min == "undefined")) {
-    if (!isNaN(parseInt(req.query.min))) min = parseInt(req.query.min);
+    if (!isNaN(parseInt(req.query.min.toString()))) min = parseInt(req.query.min.toString());
   }
   let max = 100;
   if (!(typeof req.query.max == "undefined")) {
-    if (!isNaN(parseInt(req.query.max))) max = parseInt(req.query.max);
+    if (!isNaN(parseInt(req.query.max.toString()))) max = parseInt(req.query.max.toString());
   }
   const diff = Math.max(max, min) - Math.min(max, min);
 
@@ -77,24 +88,24 @@ app.get("/random", function (req, res) {
 });
 
 app.use("/query", express.urlencoded());
-app.post("/query", function (req, res) {
+app.post("/query", function (req: Request, res: Response) {
   // console.log(req.body);
   res.send(req.body);
 });
 
-app.get("/rev", (req, res) => {
+app.get("/rev", (_: Request, res: Response) => {
   res.send(revision);
 });
 
 app.use("/register", express.json({ limit: "1kb" }));
-app.post("/register", async (req, res) => {
+app.post("/register", async (req: Request, res: Response) => {
   const username = req.body.username;
   const password = req.body.password;
 
   await client.connect();
   // console.log("connected to server!");
   const db = client.db(process.env.dbNAME);
-  const users = db.collection(process.env.dbCOLLECTION);
+  const users = db.collection(process.env.dbCOLLECTION!);
 
   // console.log(req.body);
   if (typeof username === "undefined") {
@@ -128,11 +139,11 @@ app.post("/register", async (req, res) => {
 });
 
 app.use("/login", express.json({ limit: "1mb" }));
-app.post("/login", async (req, res) => {
+app.post("/login", async (req: Request, res: Response) => {
   const username = req.body.username;
   const password = req.body.password;
   const db = client.db(process.env.dbNAME);
-  const users = db.collection(process.env.dbCOLLECTION);
+  const users = db.collection(process.env.dbCOLLECTION!);
   // console.log(req.body);
 
   // make sure input was given
@@ -181,10 +192,11 @@ app.post("/login", async (req, res) => {
 });
 
 app.use("/members", authCheck, express.static("members"));
-function authCheck(req, res, next) {
+function authCheck(req: Request, res: Response, next: NextFunction) {
   if (req.session.username) next();
   else res.redirect("/login");
 }
 
 console.log("running git revision: " + revision);
-app.listen(3000);
+console.log("Starting server at: http://127.1:"+SERV_PORT)
+app.listen(SERV_PORT);
